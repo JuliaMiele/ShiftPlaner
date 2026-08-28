61fb985cb498a2271c875c0657e0663fe21dcc40
Julia <MielewczykJ@bfwob.de>
2026-08-28 12:32:50 +0200

    Add tests for shift duration calculation

---
+++ ShiftPlaner.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using ShiftPlaner.Models;
+
+
+namespace ShiftPlaner.Tests
+{
+    public class WorkShiftTests
+    {
+        [Fact]
+        public void Duration_CalculatesNormalDayShift()
+        {
+            WorkShift workShift = new()
+            {
+                StartTime = new TimeOnly(8, 0),
+                EndTime = new TimeOnly(16, 30)
+
+
+            };
+
+            TimeSpan result = workShift.Duration;
+
+            Assert.Equal(new TimeSpan(8, 30 , 0), result);
+        }
+
+        [Fact]
+        public void Duration_CalculatesShiftAcrossMidnight()
+        {
+            WorkShift workShift = new()
+            {
+                StartTime = new TimeOnly(22, 0),
+                EndTime = new TimeOnly(6, 0)
+
+
+            };
+
+            TimeSpan result = workShift.Duration;
+            Assert.Equal(TimeSpan.FromHours(8), result);
+        }
+    }
+}
