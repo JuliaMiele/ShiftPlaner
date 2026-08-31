a4a9085c0bd964cc241980f0c61115d00f7ca5f7
Julia <MielewczykJ@bfwob.de>
2026-08-31 08:52:48 +0200

    Remove Privacy link from footer

--- ShiftPlaner/Program.cs
+++ ShiftPlaner/Program.cs
@@ -33,7 +33,7 @@ namespace ShiftPlaner
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=WorkShifts}/{action=Index}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
